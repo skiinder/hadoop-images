e617c7e7562c3949ea2db17f282e34c63cd7416c
skiinder <skiinder@live.cn>
2023-02-17 16:26:06 +0800

    添加zookeeper

---
+++ zookeeper/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# 启动SSH Daemon
+/sbin/sshd
+
+# 初始化zookeeper
+MY_ID="$(echo "$HOSTNAME" | awk -F "-" '{print $NF}')"
+DATA_DIR="$(grep 'dataDir' "$ZOOKEEPER_HOME"/conf/zoo.cfg | awk -F "=" '{print $2}')"
+mkdir -p "$DATA_DIR"
+echo "$MY_ID" > "$DATA_DIR"/myid
+
+# 启动zookeeper
+"$ZOOKEEPER_HOME/bin/zkServer.sh" "start-foreground"
+
